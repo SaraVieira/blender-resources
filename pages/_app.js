9048c67489e96eecc1047f7c08b3a1b3834b9922
Sara Vieira <hey@iamsaravieira.com>
2020-11-30 00:41:31 +0100

    add next on netlifly

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,11 @@
 import React from "react";
 import App from "next/app";
-import { Devtools } from "@ui-devtools/tailwind";
 import "../styles/tailwind.css";
 
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
-    return (
-      <Devtools>
-        <Component {...pageProps} />
-      </Devtools>
-    );
+    return <Component {...pageProps} />;
   }
 }
 
