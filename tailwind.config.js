0c93d3d4f8951482d559caf74a5dfa97295f7382
Sara Vieira <hey@iamsaravieira.com>
2020-12-06 00:48:27 +0100

    add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,12 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
   purge: ["./pages/**/*.js", "./components/**/*.js"],
   plugins: [require("@tailwindcss/forms")],
 };
